a737f70ab02bc57af77b963b175c08625f0c48cd
apxilleaneaadijuliano-arch <ap.xilleaneaadi.juliano@apecschools.edu.ph>
2026-02-08 23:27:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,24 @@ setInterval(() => {
   headers.forEach(h => {
     h.style.color = `hsl(${Math.floor(Math.random() * 360)}, 70%, 60%)`;
   });
-}, 3000);
+}, 2500);
 
 // Glow effect for nav links
 document.querySelectorAll('nav a').forEach(link => {
   link.addEventListener('mouseover', () => {
-    link.style.boxShadow = "0 0 15px #fff";
+    link.style.boxShadow = "0 0 20px #fff";
   });
   link.addEventListener('mouseout', () => {
     link.style.boxShadow = "none";
+  });
+});
+
+// Smooth scroll for internal anchors (future-proof if you add #sections)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
   });
 });
